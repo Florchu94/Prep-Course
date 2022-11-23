10658c2a82a508afd3500b93a4ae77df99929161
florchu94 <florencia_box@hotmail.com>
2022-11-23 02:55:43 -0300

    okok

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -180,7 +180,10 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  if (valor === true) {
+    return "Soy verdadero";
+  }
+    return "soy falso";
 }
 
 function tablaDelSeis(){
